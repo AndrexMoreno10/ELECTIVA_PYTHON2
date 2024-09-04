7dcdc9a7cb5df25d1aebbc40f7ff4076ce1bed43
AndrexMoreno10 <adp2333@gmail.com>
2024-09-04 15:55:49 -0500

    Commit con el Taller 1 de las 5 entidades

---
+++ app/main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+from starlette.responses import RedirectResponse
+
+#Routers
+from .routes.user import users_route
+from .routes.animal import animals_route
+from .routes.vehicle import vehicles_route
+from .routes.product import products_route
+from .routes.venta import ventas_route
+
+
+app = FastAPI()
+
+#On Startup
+#On Shutdown
+
+#Documentation
+@app.get("/")
+async def docs():
+    return RedirectResponse(url="/docs")
+
+#localhost:9090/
+#Rutas
+app.include_router(users_route, prefix="/users", tags=["Users"])
+app.include_router(animals_route, prefix="/animals", tags=["Animals"])
+app.include_router(vehicles_route, prefix="/vehicles", tags=["Vehicles"])
+app.include_router(products_route, prefix="/products", tags=["Products"])
+app.include_router(ventas_route, prefix="/ventas", tags=["Ventas"])
